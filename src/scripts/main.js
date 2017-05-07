28441233989562d9668bf5e4a329c56630f04ec0
Dmitry <irvis@imega.ru>
2017-05-07 12:52:58 +0300

    add custom settings

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,9 @@ import "foundation-sites/dist/js/plugins/foundation.util.motion.min"
 import "foundation-sites/dist/js/plugins/foundation.util.mediaQuery.min"
 import "foundation-sites/dist/js/plugins/foundation.util.keyboard.min"
 import "foundation-sites/dist/js/plugins/foundation.accordionMenu.min"
+import "foundation-sites/dist/js/plugins/foundation.orbit.min"
+import "foundation-sites/dist/js/plugins/foundation.util.timerAndImageLoader.min"
+import "foundation-sites/dist/js/plugins/foundation.util.touch.min"
+import "motion-ui/dist/motion-ui.min"
 
 $(document).foundation();
-
