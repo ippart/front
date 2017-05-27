a8c987fb3874ad683adff941423c0a82c979676e
Dmitry <irvis@imega.ru>
2017-05-27 16:48:39 +0300

    add product detail

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,5 +8,10 @@ import "foundation-sites/dist/js/plugins/foundation.orbit.min"
 import "foundation-sites/dist/js/plugins/foundation.util.timerAndImageLoader.min"
 import "foundation-sites/dist/js/plugins/foundation.util.touch.min"
 import "motion-ui/dist/motion-ui.min"
+import "foundation-sites/dist/js/plugins/foundation.tabs.min"
 
 $(document).foundation();
+
+$('.sim-thumb').on('click', function() {
+    $('#main-product-image').attr('src', $(this).data('image'));
+})
